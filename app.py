eb404fd9778c948bda1dabb954b0288ccbbfb2a6
Glsil <144020290+Glsil@users.noreply.github.com>
2023-09-08 16:30:06 -0300

    update_0.2

--- app.py
+++ app.py
@@ -2,17 +2,21 @@ from flask import Flask, jsonify, request
 import json
 
 
-with open("json.json", encoding='utf-8') as meu_json:
-    dados = json.load(meu_json)
+with open("json.json", encoding='utf-8') as json_file:
+    data = json.load(json_file)
 
 app = Flask(__name__)
 
 
-@app.route('/data')
-def obter_contatos():
-    return jsonify(dados)
+@app.route('/data',methods=['GET'])
+def contatos():
+    return jsonify(data)
 
-
+@app.route('/datapost',methods=['POST'])
+def add_register():
+    new_register = request.get_json()
+    data.append(new_register)
+    return data
 
 
 app.run(port=5000,host='localhost',debug=True)
