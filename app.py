2a21f1b00bfa8d343069ea81a7f8e7079181c0a3
Geovane L. Silva <115097827+Galsz@users.noreply.github.com>
2023-09-12 16:24:35 -0300

    Update_0.5

--- app.py
+++ app.py
@@ -12,11 +12,27 @@ app = Flask(__name__)
 def contatos():
     return jsonify(data)
 
+@app.route('/data/<string:id>',methods=['GET'])
+def atendimentos_id(id):
+    for atendimento in data["Atendimentos"]:
+        if atendimento.get('AtendimentoCodigo') == id:
+            return jsonify(atendimento)
+
+
 @app.route('/datapost',methods=['POST'])
 def add_register():
     new_register = request.get_json()
     data["Atendimentos"].append(new_register)
     return data
+
+
+@app.route('/data/<string:id>',methods=['PUT'])
+def update_register_id(id):
+    update_register = request.get_json()
+    for i,atendimento in enumerate(data["Atendimentos"]):
+        if atendimento.get('AtendimentoCodigo') == id:
+            data["Atendimentos"][i].update(update_register)
+            return jsonify(data["Atendimentos"][i])
 
 
 app.run(port=5000,host='localhost',debug=True)
